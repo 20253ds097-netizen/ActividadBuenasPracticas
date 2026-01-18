403b44ff72372cdeed393355b1417901bab1ed18
20253ds097-netizen <20253ds097@utez.edu.mx>
2026-01-18 13:31:24 -0600

    Primer commit: Código corregido siguiendo buenas prácticas

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numeroLimite = pedirEntero();
+        int resultadoSuma = sumarHastaN(numeroLimite);
+        System.out.println("La suma de todos los números desde 1 hasta " + numeroLimite + " es: " + resultadoSuma);
+    }
+    public static int pedirEntero() {
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Por favor, ingresa un número entero positivo: ");
+        while (!lector.hasNextInt()) {
+            System.out.println("Eso no parece un número. Intenta de nuevo:");
+            lector.next();
+        }
+        return lector.nextInt();
+    }
+    public static int sumarHastaN(int n) {
+        int sumaAcumulada = 0;
+        for (int i = 1; i <= n; i++) {
+            sumaAcumulada += i;
+        }
+        return sumaAcumulada;
+    }
+}
